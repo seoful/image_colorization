721e7d6d32437e2440ffa8a7ad58ad2afdc63c0e
seoful <seoful@yandex.ru>
2023-11-26 21:25:28 +0300

    added oklab

--- dataset.py
+++ dataset.py
@@ -9,6 +9,7 @@ from PIL import Image
 import numpy as np
 import cv2
 from skimage.color import rgb2lab, lab2rgb
+import colour
 
 
 
@@ -35,14 +36,36 @@ class ColorizationDataset(Dataset):
         img_lab = transforms.ToTensor()(img_lab)
         L = img_lab[[0], ...] / 50. - 1. # Between -1 and 1
         ab = img_lab[[1, 2], ...] / 110. # Between -1 and 1
-
+        print("meow")
         return {'L': L, 'ab': ab}
 
     def __len__(self):
         return len(self.paths)
 
-def make_dataloaders(batch_size=60, n_workers=4, pin_memory=True, **kwargs): # A handy function to make our dataloaders
-    dataset = ColorizationDataset(**kwargs)
+
+class OkLabColorizationDataset(ColorizationDataset):
+    def __getitem__(self, idx):
+        img = Image.open(self.paths[idx]).convert("RGB")
+        img = self.transforms(img)
+        img = np.array(img) / 255
+        xyz = colour.sRGB_to_XYZ(img)
+
+        oklab = colour.XYZ_to_Oklab(xyz).astype('float32')
+        oklab = transforms.ToTensor()(oklab)
+        L = oklab[[0], ...]
+        ab = oklab[[1, 2], ...]
+
+        return {'L': L, 'ab': ab}
+
+        # img_lab = rgb2lab(img).astype("float32") # Converting RGB to L*a*b
+        # img_lab = transforms.ToTensor()(img_lab)
+        # L = img_lab[[0], ...] / 50. - 1. # Between -1 and 1
+        # ab = img_lab[[1, 2], ...] / 110. # Between -1 and 1
+
+
+
+def make_dataloaders(batch_size=16, n_workers=4, pin_memory=True, **kwargs): # A handy function to make our dataloaders
+    dataset = OkLabColorizationDataset(**kwargs)
     dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=n_workers,
                             pin_memory=pin_memory)
     return dataloader
